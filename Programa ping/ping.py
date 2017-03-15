c613ace229371388412a4c510de263b4e663c3ef
rockrubio666 <monroyrubioc@gmail.com>
2017-03-14 21:53:32 -0600

    Corregido

--- Programa ping/ping.py
+++ Programa ping/ping.py
@@ -18,13 +18,13 @@ for line in ttl:
 		algo = ttl1.group()
 		a,b=algo.split('=')
 		
-		if  30 < int(b) > 40:
+		if  int(b)>30 and int(b) <40:
 			print( 'Posibles sistemas: AIX, DEC Pathworks,HP-UX,OSF/1,Stratus,Ultrix,VMS/Wollongong\n')
-		elif 50 < int(b) > 70:
+		elif  int(b) > 50 and int(b) < 70:
 			print( 'Posibles sistemas: AIX,Compa,FreeBSD,HP-UX,Irix,Linux,MACOS,OpenBSD,Solaris,Stratus,SunOS,Ultrix,VMS\Multinet\n')
-		elif 100 < int(b) > 140:
+		elif int(b) > 100  and int(b) < 140:
 			print( 'Posibles sistemas: VMS/Wollongong,VMS/UCX, Windows Server, 98,2003, XP, Vista, 7, 10\n')   
-		elif 240 < int(b) > 255:
+		elif int(b) >240 and int(b) < 255:
 			print('Posibles sistemas: AIX,BSDI,FreeBSD,Irix,Linux,NetBSD,Solaris\n')
 		else:
 			print 'No hubo calsificacion'
